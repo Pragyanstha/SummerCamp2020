aa1eff6680ec70a438ece4de35a24d8b6f73af37
matsubaranaoto <55867049+matsubaranaoto@users.noreply.github.com>
2020-09-25 10:24:00 +0900

    Create edge_canny.py

---
+++ src/edge_detection/edge_canny.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys
+sys.path.append('../')
+from utils import bmp2tensor as bmp
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+
+from PIL import Image
+
+voxels=np.array(bmp.bmp2tensor(1,[100,100,100])).astype(np.uint8)
+
+
+for num in range(voxels.shape[0]):
+    cv2.Canny(voxels[num,:,:],100,200)
+
+for num in range(voxels.shape[1]):
+    cv2.Canny(voxels[:,num,:],100,200)
