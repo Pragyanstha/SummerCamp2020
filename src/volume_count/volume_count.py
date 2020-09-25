5c5c77736eb6e2ba28c5a8dcf6715b42a53880a4
matsubaranaoto <55867049+matsubaranaoto@users.noreply.github.com>
2020-09-25 13:33:00 +0900

    volume count

--- src/volume_count/volume_count.py
+++ src/volume_count/volume_count.py
@@ -1,12 +1,21 @@
 import numpy as np
-import sys
-sys.path.append('../')
-from utils import bmp2tensor_parts as bmp
+#import sys
+
+#sys.path.append('../')
+from ..utils.bmp2tensor_parts import bmp2tensor
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import cv2
 from PIL import Image
 
-voxels=np.array(bmp.bmp2tensor(1,[100,100,100]))
+voxels=bmp2tensor("Part4",[100,100,100])
 
 print(voxels)
+
+voxels_2=np.ravel(voxels)
+
+print(voxels_2.size)
+count=np.count_nonzero(voxels_2 >100 )
+
+
+print(count)
